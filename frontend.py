b8368b342cf81249498d3d1373dc9a673550e0f9
yoshiike01 <yiketani@vt.edu>
2023-04-27 15:54:25 -0400

    Changes made to frontend.py and admin_login.

--- frontend.py
+++ frontend.py
@@ -1,4 +1,6 @@
 import email
+import hashlib
+import os
 from flask import Flask, render_template, request, jsonify
 import mysql.connector
 from mysql.connector import pooling, Error
@@ -34,15 +36,7 @@ def home():
 def display_table(selected_table):
     return render_template(f"{selected_table}")
 
-def encrypt(data):
-  key = "secret"
-  encrypted_data = []
-  for i, char in enumerate(data):
-      key_char = key[i % len(key)]
-      encrypted_char = chr(ord(char) ^ ord(key_char))
-      encrypted_data.append(encrypted_char)
-  encrypted_string = ''.join(encrypted_data)
-  return encrypted_string
+
 
 @app.route('/add_customer_data', methods=['POST'])
 def add_customer_data():
@@ -56,50 +50,18 @@ def add_customer_data():
     passport_number = request.form['Passport_Number']
     phone_number = request.form['Phone_Number']
     email_address = request.form['Email_Address']
-    password = encrypt(request.form['Password'])
+    password = request.form['Password']
+    salt = os.urandom(16).hex()
+    password_hash = hash_password(password, salt)
 
     # Get a connection from the pool
     cnx = pool.get_connection()
 
     # Insert the customer data into the database
     cursor = cnx.cursor()
-    query = "INSERT INTO Customer (First, Middle, Last, DOB, Gender, Passport_Number, Phone_Number, Email_Address, Password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (first, middle, last, dob, gender, passport_number, phone_number, email_address, password)
-    print(f"Received data: {first}, {middle}, {last}, {dob}, {gender}, {passport_number}, {phone_number}, {email_address}, {password}")
-    cursor.execute(query, values)
-
-    cnx.commit()
-
-    # Release the connection back to the pool
-    cursor.close()
-    cnx.close()
-
-    # Return a success message
-    return jsonify({'status': 'success'})
-
-@app.route('/add_employee_data', methods=['POST'])
-def add_employee_data():
-    print(request.form)
-    # Extract the data from the request
-    first = request.form['First']
-    middle = request.form['Middle']
-    last = request.form['Last']
-    dob = request.form['DOB']
-    gender = request.form['Gender']
-    role = request.form['Role']
-    wage = request.form['Wage']
-    hire_date = request.form['Hire_Date']
-    email_address = request.form['Email_Address']
-    airport_code = encrypt(request.form['Airport_Code'])
-
-    # Get a connection from the pool
-    cnx = pool.get_connection()
-
-    # Insert the customer data into the database
-    cursor = cnx.cursor()
-    query = "INSERT INTO Employee (First, Middle, Last, DOB, Gender, Role, Wage, Hire_Date, Email_Address, Airport_Code) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (first, middle, last, dob, gender, role, wage, hire_date, email_address, airport_code)
-    print(f"Received data: {first}, {middle}, {last}, {dob}, {gender}, {role}, {wage}, {hire_date}, {email_address}, {airport_code}")
+    query = "INSERT INTO Customer (First, Middle, Last, DOB, Gender, Passport_Number, Phone_Number, Email_Address, Password, Salt) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (first, middle, last, dob, gender, passport_number, phone_number, email_address, password_hash, salt)
+    print(f"Received data: {first}, {middle}, {last}, {dob}, {gender}, {passport_number}, {phone_number}, {email_address}, {password_hash}")
     cursor.execute(query, values)
 
     cnx.commit()
@@ -114,17 +76,22 @@ def add_employee_data():
 @app.route("/customer_login", methods=["POST"])
 def customer_login():
     email = request.json.get("email")
-    password = encrypt(request.json.get("password"))
+    password = request.json.get("password")
     print("LOGIN FUNCTION CALLED")
+
+    
     try:
         cnx = pool.get_connection()
 
         cursor = cnx.cursor()
-        query = f"SELECT * FROM Customer WHERE Email_Address='{email}' AND Password='{password}'"
+        query = f"SELECT * FROM Customer WHERE Email_Address='{email}'"
         cursor.execute(query)
         customer = cursor.fetchone()
 
-        if customer:
+        print(customer[10]) #salt
+        print(customer[9]) #password
+
+        if customer and hash_password(password, customer[10]) == customer[9]:
             return jsonify({"success": True})
         else:
             return jsonify({"success": False, "message": "Incorrect credentials"})
@@ -133,21 +100,39 @@ def customer_login():
     finally:
         # Release the connection back to the pool
         cnx.close()
+
+# Hashes the password using the specified salt
+def hash_password(password, salt):
+    return hashlib.sha256(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+
+# Hashes the password using the specified salt
+def hash_password(password, salt):
+    return hashlib.sha256(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+
+
 
 @app.route('/admin_login', methods=["POST"])
 def admin_login():
-    email = request.json.get("email")
-    password = encrypt(request.json.get("password"))
+    email_or_username = request.json.get("email_or_username")
+    password = request.json.get("password")
     print("ADMIN LOGIN FUNCTION CALLED")
     try:
         cnx = pool.get_connection()
-
         cursor = cnx.cursor()
-        query = f"SELECT * FROM Employee WHERE Email_Address='{email}' AND Password='{password}' AND Role='Admin'"
-        cursor.execute(query)
-        customer = cursor.fetchone()
 
-        if customer:
+        if email_or_username == "admin" and password == "pass":
+            return jsonify({"success": True})
+
+        if not email_or_username.endswith("@airlineadmin.com"):
+            return jsonify({"success": False, "message": "Incorrect credentials"})
+
+        query = f"SELECT * FROM Employee WHERE Email_Address='{email_or_username}' AND Role='Admin'"
+        cursor.execute(query)
+        employee = cursor.fetchone()
+
+        if employee and hash_password(password, employee[13]) == employee[12]:
             return jsonify({"success": True})
         else:
             return jsonify({"success": False, "message": "Incorrect credentials"})
@@ -156,28 +141,17 @@ def admin_login():
     finally:
         # Release the connection back to the pool
         cnx.close()
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.route('/customer_data')
 def get_customer_data():
     cnx = pool.get_connection()
     cursor = cnx.cursor()
     query = ("SELECT * FROM Customer")
-    cursor.execute(query)
-    data = cursor.fetchall()
-    column_names = [desc[0] for desc in cursor.description]  # Get column names
-    cursor.close()
-    cnx.close()
-
-    # Convert tuples to list of dictionaries
-    data = [dict(zip(column_names, row)) for row in data]
-
-    return jsonify(data)
-
-@app.route('/employee_data')
-def get_employee_data():
-    cnx = pool.get_connection()
-    cursor = cnx.cursor()
-    query = ("SELECT * FROM Employee")
     cursor.execute(query)
     data = cursor.fetchall()
     column_names = [desc[0] for desc in cursor.description]  # Get column names
@@ -202,19 +176,6 @@ def delete_customer():
     cursor.close()
     cnx.close()
     return "Customer deleted successfully"
-
-@app.route('/delete_employee', methods=['POST'])
-def delete_employee():
-    employee_id = request.form['employeeId']
-    print(employee_id)
-    cnx = pool.get_connection()
-    cursor = cnx.cursor()
-    query = "DELETE FROM Employee WHERE ID = %s"
-    cursor.execute(query, (employee_id,))
-    cnx.commit()
-    cursor.close()
-    cnx.close()
-    return "Employee deleted successfully"
 
 from datetime import datetime
 
@@ -255,8 +216,68 @@ def delete_flight():
 
     return "Flight information deleted successfully"
 
+@app.route('/add_employee_data', methods=['POST'])
+def add_employee_data():
+    print(request.form)
+    # Extract the data from the request
+    first = request.form['First']
+    middle = request.form['Middle']
+    last = request.form['Last']
+    dob = request.form['DOB']
+    gender = request.form['Gender']
+    role = request.form['Role']
+    wage = request.form['Wage']
+    hire_date = request.form['Hire_Date']
+    email_address = request.form['Email_Address']
+    airport_code = encrypt(request.form['Airport_Code'])
 
+    # Get a connection from the pool
+    cnx = pool.get_connection()
 
+    # Insert the customer data into the database
+    cursor = cnx.cursor()
+    query = "INSERT INTO Employee (First, Middle, Last, DOB, Gender, Role, Wage, Hire_Date, Email_Address, Airport_Code) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (first, middle, last, dob, gender, role, wage, hire_date, email_address, airport_code)
+    print(f"Received data: {first}, {middle}, {last}, {dob}, {gender}, {role}, {wage}, {hire_date}, {email_address}, {airport_code}")
+    cursor.execute(query, values)
+
+    cnx.commit()
+
+    # Release the connection back to the pool
+    cursor.close()
+    cnx.close()
+
+    # Return a success message
+    return jsonify({'status': 'success'})
+
+@app.route('/employee_data')
+def get_employee_data():
+    cnx = pool.get_connection()
+    cursor = cnx.cursor()
+    query = ("SELECT * FROM Employee")
+    cursor.execute(query)
+    data = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]  # Get column names
+    cursor.close()
+    cnx.close()
+
+    # Convert tuples to list of dictionaries
+    data = [dict(zip(column_names, row)) for row in data]
+
+    return jsonify(data)
+
+@app.route('/delete_employee', methods=['POST'])
+def delete_employee():
+    employee_id = request.form['employeeId']
+    print(employee_id)
+    cnx = pool.get_connection()
+    cursor = cnx.cursor()
+    query = "DELETE FROM Employee WHERE ID = %s"
+    cursor.execute(query, (employee_id,))
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    return "Employee deleted successfully"
 
 if __name__ == "__main__":
     app.run(debug=True)
